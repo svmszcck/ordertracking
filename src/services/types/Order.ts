aac31c7bc2fb03ae5b858c1c7be6e81cf797173e
Onur Demirtaş <onurdemirtas91@gmail.com>
2024-09-04 22:48:18 +0300

    separate types in the order type

--- src/services/types/Order.ts
+++ src/services/types/Order.ts
@@ -6,12 +6,39 @@ export enum OrderStatus {
   TRANSIT = "In transit",
 }
 
-export type Article = {
+type Article = {
   articleNo: string;
   articleName: string;
   articleImageUrl: string;
   quantity: number;
   price: number;
+};
+
+type CheckPoint = {
+  status_details: string;
+  event_timestamp: string;
+  status: OrderStatus;
+  country_iso3: string;
+  city: string;
+  meta?: {
+    pickup_address: string;
+    pickup_address_link: string;
+    pickup_address_map_url: string;
+  };
+};
+
+type DeliveryInfo = {
+  articles: Array<Article>;
+  orderNo: string;
+  order_date: string;
+  recipient: string;
+  recipient_notification: string;
+  email: string;
+  street: string;
+  city: string;
+  region: string;
+  timezone: string;
+  announced_delivery_date: string;
 };
 
 export type Order = {
@@ -20,31 +47,8 @@ export type Order = {
   tracking_number: string;
   created: string;
   updated: string;
-  checkpoints: Array<{
-    status_details: string;
-    event_timestamp: string;
-    status: OrderStatus;
-    country_iso3: string;
-    city: string;
-    meta?: {
-      pickup_address: string;
-      pickup_address_link: string;
-      pickup_address_map_url: string;
-    };
-  }>;
-  delivery_info: {
-    articles: Array<Article>;
-    orderNo: string;
-    order_date: string;
-    recipient: string;
-    recipient_notification: string;
-    email: string;
-    street: string;
-    city: string;
-    region: string;
-    timezone: string;
-    announced_delivery_date: string;
-  };
+  checkpoints: CheckPoint[];
+  delivery_info: DeliveryInfo;
   destination_country_iso3: string;
   zip_code: string;
 };
