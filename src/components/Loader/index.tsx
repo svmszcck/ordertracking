84c3ca5534aef1147aadda80a3c6b1f31624a9e4
Onur Demirtaş <onurdemirtas91@gmail.com>
2024-09-05 18:51:02 +0300

    add missing aria attribute

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -6,7 +6,11 @@ import Styled from "./styles";
 const Loader = () => {
   return (
     <Styled>
-      <BulletList backgroundColor={Colors.GRAY_DARKER} className="loader" />
+      <BulletList
+        backgroundColor={Colors.GRAY_DARKER}
+        className="loader"
+        aria-label="Page is loading"
+      />
     </Styled>
   );
 };
